147812159ad17d65abe9cbbc9ad1e47a63eb2220
timhausweiler <haustimp@gmail.com>
2022-01-11 17:00:46 +0100

    first stylings

--- squirrel-safari/src/App.js
+++ squirrel-safari/src/App.js
@@ -14,17 +14,17 @@ function App() {
     <div className="App">
       <Routes>
         <Route path="/" element={
-          <div>
+          <div className ="body">
             <img
               src= {"https://i.imgur.com/RK3UbZi.jpeg"}
               alt="Logo of Squirrel Safari"
               className="logo"
             />
             <h1>Welcome to Squirrel Safari</h1>
-            <h4>Squirrel Safari allows you to take a (virtual) walk through Central Park to learn about the local squirrel population!</h4>
+            <p>Squirrel Safari allows you to take a (virtual) walk through Central Park to learn about the local squirrel population!</p>
             <TakeWalkButton click={click} setClick={ setClick}/>
             <br/>
-            <Link to ="/contribute" >Or contribute your own observation to our collection</Link>
+            <Link to="/contribute" >Or contribute your own observation to our collection</Link>
           </div>
         } />
         <Route path="/contribute" element={<Contribute />} />
