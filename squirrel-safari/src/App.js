c82a7fd06eb9d27b921f6bf7be57d4ba1110b84e
timhausweiler <haustimp@gmail.com>
2022-01-11 12:58:19 +0100

    bug fix on Home

--- squirrel-safari/src/App.js
+++ squirrel-safari/src/App.js
@@ -7,18 +7,19 @@ import TakeWalkButton from './Components/TakeWalkButton';
 function App() {
   return (
     <div className="App">
-      <div>
-        <img
-          src= {"https://i.imgur.com/RK3UbZi.jpeg"}
-          alt="Logo of Squirrel Safari"
-        />
-        <h1>Welcome to Squirrel Safari</h1>
-        <h4>Squirrel Safari allows you to take a (vritual) walk through Central Park to learn about the local squirrel population!</h4>
-        <TakeWalkButton />
-        <p>Or contribute your own story to our collection</p>
-      </div>
       <Routes>
-        {/* <Route path="/" element={<Home />} /> */}
+        <Route path="/" element={
+          <div>
+            <img
+              src= {"https://i.imgur.com/RK3UbZi.jpeg"}
+              alt="Logo of Squirrel Safari"
+            />
+            <h1>Welcome to Squirrel Safari</h1>
+            <h4>Squirrel Safari allows you to take a (vritual) walk through Central Park to learn about the local squirrel population!</h4>
+            <TakeWalkButton />
+            <p>Or contribute your own story to our collection</p>
+          </div>
+        } />
         <Route path="/takewalk" element={<TakeWalk />} />
       </Routes>
     </div>
