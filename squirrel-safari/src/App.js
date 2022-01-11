32ce166bc24fc45e0e715cd23abfe4f8943790a1
timhausweiler <haustimp@gmail.com>
2022-01-11 12:27:37 +0100

    import react-router-dom and Home.jsx

--- squirrel-safari/src/App.js
+++ squirrel-safari/src/App.js
@@ -1,8 +1,13 @@
 import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import Home from './Components/Home';
 
 function App() {
   return (
     <div className="App">
+      <Routes>
+        <Route path="/" element={<div>Hello, welcome to our GOT website!</div>} />
+      </Routes>
     </div>
   );
 }
