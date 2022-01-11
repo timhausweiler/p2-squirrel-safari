0e5eaf9001ef48e3f9e85bd8ca96bd714891742f
timhausweiler <haustimp@gmail.com>
2022-01-11 12:46:42 +0100

    initialize button component

---
+++ squirrel-safari/src/Components/Button.jsx
@@ -0,0 +1,13 @@
+import { useNavigate } from 'react-router-dom';
+import React from 'react'
+
+export default function Button(props) {
+
+  const navigate = useNavigate();
+
+  return (
+    <div>
+      
+    </div>
+  )
+}
