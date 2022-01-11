bd1a63b157d7775898b7e9de5dc70daa104f5a64
timhausweiler <haustimp@gmail.com>
2022-01-11 12:44:52 +0100

    placeholders in Home component

--- squirrel-safari/src/Components/Home.jsx
+++ squirrel-safari/src/Components/Home.jsx
@@ -6,7 +6,11 @@ export default function Home() {
       <img
         src= {"https://i.imgur.com/RK3UbZi.jpeg"}
         alt="Logo of Squirrel Safari"
-        />
+      />
+      <h1>Welcome to Squirrel Safari</h1>
+      <h4>Squirrel Safari allows you to take a (vritual) walk through Central Park to learn about the local squirrel population!</h4>
+      <button>Take a walk</button>
+      <p>Or contribute your own story to our collection</p>
     </div>
   )
 }
