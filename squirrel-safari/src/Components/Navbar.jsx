5d9dd3432971871c2e93264b8734741a2e99fca3
timhausweiler <haustimp@gmail.com>
2022-01-14 15:24:24 +0100

    remove logo font

--- squirrel-safari/src/Components/Navbar.jsx
+++ squirrel-safari/src/Components/Navbar.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom"
 export default function Navbar(props) {
   return (
     <div className = "navbar">
-      <div className = "nav-img-container"><Link to="/" ><img src = "https://i.imgur.com/RK3UbZi.jpeg" className = "nav-img"></img> <div>Squirrel Safari</div></Link></div>
+      <div className = "nav-img-container"><Link to="/" ><img src = "https://i.imgur.com/RK3UbZi.jpeg" className = "nav-img"></img></Link></div>
       <div className="nav-txt-container">
         <Link to={`/story/${props.nextId}`} className = "nav-text">Take a walk</Link>
         <Link to="/storylist" className="nav-text">All stories</Link>
